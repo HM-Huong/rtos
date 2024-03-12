9236cf1679ae6cd1acdd61a9c97a96d35134c1bd
HM-Huong <hmhuong1408@outlook.com>
2024-03-12 09:28:24 +0700

    task time slice

--- esp32.ino
+++ esp32.ino
@@ -1,41 +1,27 @@
-#define LED1 12
-#define LED2 2
-#define LED3 14
+#define GPIO 2
 
-struct s_led {
-	byte gpio;
-	byte state;
-	unsigned napms; // nap time in milliseconds
-	TaskHandle_t task;
-};
+/*
+In a single-core MCU (Micro-Controller Unit), only one task can execute at any instance in time. The task that is executing runs until a hardware timer indicates that the time slice has expired. At timeout, the FreeRTOS scheduler saves the state of the current task by saving its registers. The current task is said to have been preempted by the timer.
 
-static struct s_led leds[] = {
-	{LED1, 0, 500, 0},
-	{LED2, 0, 200, 0},
-	{LED3, 0, 750, 0}};
+The scheduler then chooses another task that is ready to run. The state of the highest priority task, which is ready to run, is restored and resumed where it left off. The duration of the time slice is small enough, that the MCU can run several tasks per second. This is known as concurrent processing.
+*/
 
-static void led_task(void *argp) {
-	struct s_led *led = (struct s_led *)argp;
-	unsigned stack_hwm = 0, tmp;
-	// printf("Task for GPIO %d is running on core %d\n", led->gpio, xPortGetCoreID());
-	delay(1000); // allow the setup() to finish before the task gets underway
+static void gpioOn(void *pvParameters) {
 	while (1) {
-		digitalWrite(led->gpio, led->state ^= 1);
-		// get the number of free stack bytes
-		tmp = uxTaskGetStackHighWaterMark(NULL);
-		if (!stack_hwm || tmp < stack_hwm) {
-			stack_hwm = tmp;
-			// if there was no call to printf in the previous,
-			// in the next report, the free stack bytes
-			// will be decremented because of the call
-			// to printf (which uses the stack)
-			printf("Task for GPIO %d has stack HWM of %u bytes, free heap = %u\n", led->gpio, stack_hwm, xPortGetFreeHeapSize());
-		}
-		vTaskDelay(pdMS_TO_TICKS(led->napms));
+		digitalWrite(GPIO, HIGH);
 	}
 }
 
-// the setup and loop functions belong to the loopTask (has priority 1)
+static void gpioOff(void *pvParameters) {
+	while (1) {
+		digitalWrite(GPIO, LOW);
+	}
+}
+
+// when running
+// loopTask has priority 1
+// IDLE1 task has priority 0
+// ipc1 task has priority 24, but it often waits for an event
 void setup() {
 	delay(1000); // wait for the serial to be ready
 	printf("----- setup\n");
@@ -44,21 +30,12 @@ void setup() {
 	int app_cpu = xPortGetCoreID();
 	printf("app_cpu = %d\n", app_cpu);
 
-	for (auto &led : leds) {
-		pinMode(led.gpio, OUTPUT);
-		digitalWrite(led.gpio, led.state);
-		xTaskCreatePinnedToCore(led_task, "led_task", 2048, &led, 1, &led.task, app_cpu);
-		printf("Created task for GPIO %d\n", led.gpio);
-	}
+	pinMode(GPIO, OUTPUT);
+	xTaskCreatePinnedToCore(gpioOn, "gpioOn", 2048, NULL, 1, NULL, app_cpu);
+	xTaskCreatePinnedToCore(gpioOff, "gpioOff", 2048, NULL, 1, NULL, app_cpu);
 	printf("----- setup done\n");
-	printf("There're %u heap bytes available\n", xPortGetFreeHeapSize());
 }
 
 void loop() {
-	delay(5000);
-	printf("Suspending middle LED task (GPIO %d)\n", leds[1].gpio);
-	vTaskSuspend(leds[1].task);
-	delay(5000);
-	printf("Resuming middle LED task (GPIO %d)\n", leds[1].gpio);
-	vTaskResume(leds[1].task);
+	vTaskDelete(xTaskGetCurrentTaskHandle());
 }
